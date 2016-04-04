0e9b7e6381beddba03efee0a1043deaaaed40b23
Travis Augustine <augustine@ku.edu>
2016-04-04 09:43:15 -0500

    Add mysql functionality

--- SignUpBackEnd.php
+++ SignUpBackEnd.php
@@ -1,8 +1,24 @@
 <?php
+
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "", "", "");
+
 	$firstName = $_POST["firstName"];
 	$lastName = $_POST["lastName"];
 	$email = $_POST["email"];
 	$password = $_POST["passwordFirst"];
+
+$sql = "INSERT INTO EECSUsers (FirstName, LastName, Email, Password) VALUES ('$firstName', '$lastName', '$email', '$password')";
+
+if($mysqli->query($sql) === TRUE) {
+	echo "New record created successfully";
+	}
+else{
+	echo "Error: " . $sql . "<br>" . $mysqli->error;
+}
+
+}
+/* close connection */
+$mysqli->close();
 
 
 ?>
