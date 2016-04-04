b6e55abb6557854ff33039d866b750089f91c381
Travis Augustine <augustine@ku.edu>
2016-04-04 10:24:35 -0500

    Add check for username

---
+++ LoginBackEnd.php
@@ -0,0 +1,33 @@
+<?php
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "eward", "ethanward", "eward");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$my_query = "SELECT * FROM EECSUsers WHERE user_id = '$username'";
+$result = mysqli_query($mysqli, $my_query);
+$row_num = mysqli_num_rows($result);
+
+if($row_num == 1)
+{
+
+}
+else
+{
+	echo "Error: Username does not exist";
+
+}
+
+/* close connection */
+$mysqli->close();
+?>
+
+
+?>
