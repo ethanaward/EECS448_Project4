a4458a7ed00836679cb57d41cbac27cf8e5798b9
Ethan Ward <ethanandrewward@gmail.com>
2016-04-06 14:06:40 -0500

    Add basic table for profile page

--- ProfileBackEnd.php
+++ ProfileBackEnd.php
@@ -1,18 +1,24 @@
 <?php
 
-$mysqli = new mysqli("mysql.eecs.ku.edu", "eward", "ethanward", "eward");
+$mysqli = new mysqli('mysql.eecs.ku.edu', 'eward', 'ethanward', 'eward');
 
 if($mysqli->connect_errno) {
   printf("Connect failed: %s\n", $mysqli->connect_error);
   exit();
 }
 
-$query = "SELECT Email FROM EECSUsers WHERE user_id ='mike'";
+$query = "SELECT * FROM EECSUsers";
 
 if($result = $mysqli->query($query)) {
+  echo "<table>";
+	while($row = $result->fetch_assoc()) {
 
-	$row = $result->fetch-assoc();
-      printf("%s<br>", $row["Email]);
-
+		printf("<tr> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> </tr>", $row["user_id"], $row["Email"], $row["FirstName"], $row["LastName"]);
+	}
+  echo "</table>";
+	$result->free();
 }
->
+
+$mysqli->close();
+
+?>
