a21145f3c4c78d1b295bb0de4406684b2191367c
Ethan Ward <ethanandrewward@gmail.com>
2016-04-06 02:15:07 -0500

    Add basic functionality for Profile

--- ProfileBackEnd.php
+++ ProfileBackEnd.php
@@ -1,3 +1,17 @@
 <?php
 
->
+$mysqli = new mysqli("mysql.eecs.ku.edu", "eward", "ethanward", "eward");
+
+if($mysqli->connect_errno) {
+  printf("Connect failed: %s\n", $mysqli->connect_error);
+  exit();
+}
+
+$query = "SELECT * FROM EECSUsers WHERE user_id ='mike'";
+
+if($result = $mysqli->query($query)) {
+
+
+      echo($result["Email"]<br);
+
+}
