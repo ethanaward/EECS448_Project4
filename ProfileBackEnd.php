e1d8cbb37469d13483ce69cd17519e45cf26a0cc
Ethan Ward <ethanandrewward@gmail.com>
2016-04-08 17:19:25 -0500

    Make profile pages work with the new class

--- ProfileBackEnd.php
+++ ProfileBackEnd.php
@@ -1,55 +1,10 @@
 <?php
 
 session_start();
-$mysqli = new mysqli('mysql.eecs.ku.edu', 'eward', 'ethanward', 'eward');
+include "Profile.php";
 
-if($mysqli->connect_errno) {
-  printf("Connect failed: %s\n", $mysqli->connect_error);
-  exit();
-}
+$profile = new Profile();
 
-
-$query = "SELECT * FROM EECSUsers WHERE user_id = '".$_SESSION['profilename']."'";
-
-
-if($_SESSION['username'] == $_SESSION['profilename']) {
-
-    if($result = $mysqli->query($query)) {
-    echo "<form action = 'EditProfile.php' method = 'post'>";
-      echo "<table>";
-      while($row = $result->fetch_assoc()) {
-
-        printf("<tr> <td>Username:</td><td> %s</td> </tr>
-        <tr> <td>Email address:</td><td><input type = 'text' name = 'email' value = '%s'></td> </tr>
-        <tr> <td>First Name:</td><td> <input type = 'text' name = 'firstName' value = '%s'></td> </tr>
-        <tr> <td>Last Name:</td><td><input type = 'text' name = 'lastName' value = '%s'></td> </tr>", $row["user_id"], $row["Email"], $row["FirstName"], $row["LastName"]);
-      }
-      echo "</table>";
-      echo "<input type = 'submit' value = 'Edit profile'>";
-      echo "</form>";
-      $result->free();
-
-
-      $mysqli->close();
-    }
-}
-
-else {
-  if($result = $mysqli->query($query)) {
-
-    echo "<table>";
-    while($row = $result->fetch_assoc()) {
-
-      printf("<tr> <td>Username:</td><td> %s</td> </tr>
-      <tr> <td>Email address:</td><td>%s</td> </tr>
-      <tr> <td>First Name:</td><td>%s</td> </tr>
-      <tr> <td>Last Name:</td><td>%s</td> </tr>", $row["user_id"], $row["Email"], $row["FirstName"], $row["LastName"]);
-    }
-    echo "</table>";
-    $result->free();
-
-    $mysqli->close();
-  }
-}
+$profile->display();
 
  ?>
