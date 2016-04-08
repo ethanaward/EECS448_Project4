8dc8e19b23963a0cebf78386e834f333915b618c
Ethan Ward <ethanandrewward@gmail.com>
2016-04-08 12:34:03 -0500

    Add php files to check before displaying links

---
+++ DisplayLogoutLink.php
@@ -0,0 +1,4 @@
+<?php if(isset($_SESSION['username'])) {
+  echo "<a href='LogoutFrontEnd.html'>Log out</a>
+  <br>";
+} ?>
---
+++ DisplayProfileLink.php
@@ -0,0 +1,4 @@
+<?php if(isset($_SESSION['username'])) {
+  echo "<a href='ProfileFrontEnd.html'>Your Profile</a>
+  <br>";
+} ?>
