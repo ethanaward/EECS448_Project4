864ca5abcb7d48ea79e124a1f471ddbfac2602ef
NeisesMike <neisesmichael@gmail.com>
2016-04-08 14:42:13 -0500

    Add Comments

--- FeedBackEnd.php
+++ FeedBackEnd.php
@@ -6,10 +6,14 @@
 **/
 session_start();
 
+//This variable is used to store the user_id of the poster
 $user = $_POST["user"];
 
+//This is the main html display for the feed.
+//You can place new, additional titles here in the future
 echo "<h1>Main Feed</h1>";
 
+//Initialize the database connection
 $mysqli = new mysqli('mysql.eecs.ku.edu', 'eward', 'ethanward', 'eward');
 
 /* check connection */
@@ -18,19 +22,22 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
+//Here we select all things from the EECSPosts database.
 $query = "SELECT * FROM EECSPosts";
 
 if ($result = $mysqli->query($query)) {
     /* fetch associative array */
-        echo "<ul>";
+    
+    //Here we display the posts with the related username, which also serves as a link to their profile page.
+    echo "<ul>";
     while ($row = $result->fetch_assoc()) {
-                echo "<br>";
+    	echo "<br>";
         printf ("%s \n", $row["content"]);
-                echo "<br>-";
+        echo "<br>-";
         printf ("<a href = 'ProfileFrontEnd.html?profile=%s'>%s</a> \n", $row["user_id"],$row["user_id"]);
-                echo "<br>";
+        echo "<br>";
     }
-        echo "</ul>";
+    echo "</ul>";
 
     /* free result set */
     $result->free();
