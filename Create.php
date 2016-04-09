f00b1aad2447967e699665c810df3bbe740970d5
Travis Augustine <augustine@ku.edu>
2016-04-08 19:44:18 -0500

    Implement create post class

---
+++ Create.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+/*
+* Author: Michael Neises
+* Date Modified: April 8, 2016
+* Purpose: to add a post to the EECSPosts database
+*/
+	class Create 
+	{
+
+		private $post;
+		private $user;
+		private $here;
+		private $query;
+		private $exists;
+		private $mysqli;
+	
+		public function Create()
+		{
+			
+			//These two variables hold the post content and the username of the poster respectively.
+			$this->post = $_POST["mypost"];
+			$this->user = $_POST["user"];
+			//This variable is used to see if the user_id exists within the EECSUsers database.
+			$this->here = false;
+			
+			//Here we initialize the connection to the database.
+			$this->mysqli = new mysqli('mysql.eecs.ku.edu', 'eward', 'ethanward', 'eward');
+
+			
+			//Here I escape the post return into a correctly formatted string.
+			//Example: without this line, if the post contains a single apostrophe ("I'm coding."),
+			//Then that apostrophe will confuse the query and the program crashes.
+			$this->post = $this->mysqli->real_escape_string($this->post);
+			
+			
+			//This query we will use to add the post with the username into the EECSPosts database.
+			$this->query = "INSERT INTO EECSPosts (content, user_id) VALUES('$this->post', '$this->user')";
+			//This quere we will use to select all user_id from the EECSUsers databse.
+			$this->exists = "SELECT user_id FROM EECSUsers";
+			
+		}
+
+		private function isOK() 
+		{
+			/* check connection */
+			if ($this->mysqli->connect_errno) 
+			{
+				printf("Connect failed: %s\n", $this->mysqli->connect_error);
+				exit();
+			}
+		}
+
+		private function userExists()
+		{
+			
+			//We try the query called "exists"
+			if ($result = $this->mysqli->query($this->exists))
+			{
+
+				//fetch associative array
+				while ($row = $result->fetch_assoc())
+				{
+				
+							//Try to match each user_id in the database to the username given by input.
+							if ($row["user_id"]==$this->user)
+							{
+									//If that user_id exists, set $here to true.
+									$this->here = true;
+							}
+				}
+
+				//free result set
+				$result->free();
+			}
+			
+		}
+
+
+		public function makePost()
+		{
+			
+			$this->isOK();
+			
+			$this->userExists();
+			
+			//See echos for explanations inside this conditional.
+			if(!$this->here) {
+					echo "Post not saved! That username does not exist!";
+			} else if($this->mysqli->query($this->query)===TRUE) {
+					echo "New post created successfully!";
+			} else {
+					echo "Error: ".$this->query."<br>".$this->mysqli->error;
+			}
+
+			//close connection
+			$this->mysqli->close();
+			
+
+		}
+		
+	}
+?>
+
+
--- CreatePosts.php
+++ CreatePosts.php
@@ -1,71 +1,15 @@
 <?php
+
 session_start();
-/*
-* Author: Michael Neises
-* Date Modified: April 8, 2016
-* Purpose: to add a post to the EECSPosts database
-*/
+include "Create.php";
 
-//These two variables hold the post content and the username of the poster respectively.
-$post = $_POST["mypost"];
-$user = $_POST["user"];
-//This variable is used to see if the user_id exists within the EECSUsers database.
-$here = false;
+$create = new Create();
 
-//Here we initialize the connection to the database.
-$mysqli = new mysqli('mysql.eecs.ku.edu', 'eward', 'ethanward', 'eward');
-
-//Here I escape the post return into a correctly formatted string.
-//Example: without this line, if the post contains a single apostrophe ("I'm coding."),
-//Then that apostrophe will confuse the query and the program crashes.
-$post = $mysqli->real_escape_string($post);
-
-
-/* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-
-//This query we will use to add the post with the username into the EECSPosts database.
-$query = "INSERT INTO EECSPosts (content, user_id) VALUES('$post', '$user')";
-//This quere we will use to select all user_id from the EECSUsers databse.
-$exists = "SELECT user_id FROM EECSUsers";
-
-//We try the query called "exists"
-if ($result = $mysqli->query($exists)) {
-
-    /* fetch associative array */
-    while ($row = $result->fetch_assoc()) {
-                
-                //Try to match each user_id in the database to the username given by input.
-                if ($row["user_id"]==$user){
-                        //If that user_id exists, set $here to true.
-                        $here = true;
-                }
-    }
-
-    /* free result set */
-    $result->free();
-}
-
-//See echos for explanations inside this conditional.
-if($user==""){
-        echo "Post not saved! Please enter a username!";
-} else if($post==""){
-        echo "Post not saved! Please enter a post!";
-} else if(!$here) {
-        echo "Post not saved! That username does not exist!";
-} else if($mysqli->query($query)===TRUE) {
-        echo "New post created successfully!";
-} else {
-        echo "Error: ".$query."<br>".$mysqli->error;
-}
-
-/* close connection */
-$mysqli->close();
+$create->makePost();
 
 ?>
+
+
 <!--
     In this html section, we simply provide a link back to the feed.
 -->
