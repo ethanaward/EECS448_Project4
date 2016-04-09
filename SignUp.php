b9ee5ea91af42fbeb88c44b84cab767f1c65dc2a
Travis Augustine <augustine@ku.edu>
2016-04-08 20:29:31 -0500

    Add class structure for signup

---
+++ SignUp.php
@@ -0,0 +1,58 @@
+<?php
+  session_start();
+
+	class SignUp {
+	
+	private $mysqli;
+	private $username;
+	private $firstname;
+	private $lastname;
+	private $email;
+	private $password;
+	private $sql;
+	
+	public function SignUp()
+	{
+		
+		$this->mysqli = new mysqli("mysql.eecs.ku.edu", "eward", "ethanward", "eward");
+		$this->username = $_POST["username"];
+		$this->firstName = $_POST["firstName"];
+		$this->lastName = $_POST["lastName"];
+		$this->email = $_POST["email"];
+		$this->password = $_POST["passwordFirst"];
+		$this->sql = "INSERT INTO EECSUsers (user_id, FirstName, LastName, Email, Password) VALUES ('$this->username', '$this->firstName', '$this->lastName', '$this->email', '$this->password')";
+
+	}
+	
+	private function redirectPage()
+	{
+	 
+		header("Location: ProfileFrontEnd.html", TRUE, 303);
+	
+	}
+
+	public function runQuery()
+	{
+	
+		if($this->mysqli->query($this->sql))
+		{
+      		$_SESSION['username'] = $this->username;
+      		$_SESSION['profilename'] = $this->username;
+		}
+		else
+		{
+			echo "Error: " . $this->sql . "<br>" . $this->mysqli->error;
+		}
+		/* close connection */
+		$this->mysqli->close();
+		
+		$this->redirectPage();
+	
+	}
+
+	
+
+
+
+}
+?>
