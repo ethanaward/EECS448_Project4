79a657fe1e25cfa6abf105f171b367b4e3754fae
Ethan Ward <ethanandrewward@gmail.com>
2016-04-08 20:40:33 -0500

    Remove unnecessary functions

--- Create.php
+++ Create.php
@@ -12,7 +12,6 @@ session_start();
 		private $user;
 		private $here;
 		private $query;
-		private $exists;
 		private $mysqli;
 
 		public function Create()
@@ -36,8 +35,6 @@ session_start();
 
 			//This query we will use to add the post with the username into the EECSPosts database.
 			$this->query = "INSERT INTO EECSPosts (content, user_id) VALUES('$this->post', '$this->user')";
-			//This quere we will use to select all user_id from the EECSUsers databse.
-			$this->exists = "SELECT user_id FROM EECSUsers";
 
 		}
 
@@ -51,48 +48,14 @@ session_start();
 			}
 		}
 
-		private function userExists()
-		{
-			//Checks to make sure the mysql database can be accessed
-			$this->isOK();
-
-			//We try the query called "exists"
-			if ($result = $this->mysqli->query($this->exists))
-			{
-
-				//fetch associative array
-				while ($row = $result->fetch_assoc())
-				{
-
-							//Try to match each user_id in the database to the username given by input.
-							if ($row["user_id"]==$this->user)
-							{
-									//If that user_id exists, set $here to true.
-									$this->here = true;
-							}
-				}
-
-				//free result set
-				$result->free();
-			}
-
-		}
-
-
 		public function makePost()
 		{
-
+			//Test to make sure the database can be accessed
 			$this->isOK();
 
-			$this->userExists();
-
 			//See echos for explanations inside this conditional.
-			if($this->mysqli->query($this->query)===TRUE) {
-				if($this->user == $_SESSION['username']) {
+			if($this->mysqli->query($this->query)==TRUE) {
 					echo "New post created successfully!";
-				}
-				else {
-					echo "The post could not be created";
 				}
 			} else {
 					echo "Error: ".$this->query."<br>".$this->mysqli->error;
--- SignUpBackEnd.php
+++ SignUpBackEnd.php
@@ -1,9 +1,27 @@
 <?php
+  session_start();
 
-session_start();
-include "SignUp.php";
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "eward", "ethanward", "eward");
+	$username = $_POST["username"];
+	$firstName = $_POST["firstName"];
+	$lastName = $_POST["lastName"];
+	$email = $_POST["email"];
+	$password = $_POST["passwordFirst"];
 
-$signup = new SignUp();
+		$sql = "INSERT INTO EECSUsers (user_id, FirstName, LastName, Email, Password) VALUES ('$username', '$firstName', '$lastName', '$email', '$password')";
 
-$signup->runQuery();
- ?>
+		if($mysqli->query($sql))
+		{
+      $_SESSION['username'] = $username;
+      $_SESSION['profilename'] = $username;
+		}
+		else
+		{
+			echo "Error: " . $sql . "<br>" . $mysqli->error;
+		}
+
+/* close connection */
+$mysqli->close();
+
+header("Location: ProfileFrontEnd.html", TRUE, 303);
+?>
