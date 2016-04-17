9f675368a95227baf44d0b4a4000a83a2591fa44
Ethan Ward <ethanandrewward@gmail.com>
2016-04-17 15:21:05 -0500

    Add utility class for checking if a user exists or if a friend exists

---
+++ src/Utility.php
@@ -0,0 +1,53 @@
+<?php
+
+  session_start();
+
+  class Utility {
+
+  private $query;
+  private $mysqli;
+
+  public function Utility() {
+    $this->mysqli = new mysqli('mysql.eecs.ku.edu', 'eward', 'ethanward', 'eward');
+  }
+
+  private function isOK() {
+    if($this->mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $this->mysqli->connect_error);
+      exit();
+    }
+  }
+
+  public function checkUser($user) {
+    	$this->isOK();
+      $this->query = "SELECT count(1) FROM EECSUsers WHERE user_id='". $user ."'";
+        if($result = $this->mysqli->query($this->query)) {
+          $row = $result->fetch_assoc();
+          if($row['count(1)']) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        }
+
+  }
+
+  public function checkFriend($user, $friend) {
+    $this->isOK();
+    $this->query = "SELECT count(1) FROM ".$user."_Friends WHERE user_id='". $friend ."'";
+      if($result = $this->mysqli->query($this->query)) {
+        $row = $result->fetch_assoc();
+        if($row['count(1)']) {
+          return true;
+        }
+        else {
+          return false;
+        }
+      }
+
+  }
+
+  }
+
+?>
