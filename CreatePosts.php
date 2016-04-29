fa126a6c6494a0ee7f9b043c9a72fb20cb3b1000
NeisesMike <neisesmichael@gmail.com>
2016-04-29 16:08:31 -0500

    add handling for duplicate topics

--- CreatePosts.php
+++ CreatePosts.php
@@ -10,9 +10,17 @@ session_start();
 include "src/Create.php";
 
 if(isset($_SESSION['username'])) {
-  $create = new Create();
-  $create->makePost();
-  $_SESSION['message'] = "Post created!";
+	$create = new Create();
+	$exist = $create->topicExists();
+	if(!$exist)
+	{
+		$create->makePost();
+		$_SESSION['message'] = "Post created!";
+	}
+	else
+	{
+ 		$_SESSION['message'] = "That topic already exists! Find it here: ";
+	}
 }
 
 else {
