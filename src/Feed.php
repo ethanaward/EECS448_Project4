e8d8bbde10f07105b0fc291fea277f04dcf05ef2
NeisesMike <neisesmichael@gmail.com>
2016-04-29 14:11:24 -0500

    add date display

--- src/Feed.php
+++ src/Feed.php
@@ -75,7 +75,7 @@
 					echo "<br>";
 					printf ("%s \n", $row["content"]);
 					echo "<br>-";
-					printf ("<a href = 'ProfileFrontEnd.html?profile=%s'>%s</a> \n", $row["user_id"],$row["user_id"]);
+					printf ("<a href = 'ProfileFrontEnd.html?profile=%s'>%s</a> (%s)", $row["user_id"],$row["user_id"],$row["Date"]);
 					echo "<br>";
 				}
 				echo "</ul>";
