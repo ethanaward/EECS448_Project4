6d193f6ce8f57d80202e33965d7c01eb737dd25a
NeisesMike <neisesmichael@gmail.com>
2016-05-04 19:30:16 -0500

    create RunTests.php

---
+++ RunTests.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*	@file : RunTests.php
+*	@author : Mike Neises, Travis Augustine, Ethan Ward
+*	@date : 2016.04.08
+*	@brief: Creates new TestSuite object
+*/
+error_reporting(0);
+session_start();
+include "src/TestSuite.php";
+
+$TestSuite = new TestSuite();
+
+$TestSuite->RunTests();	
+
+$TestSuite->close();
+
